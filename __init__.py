f5044465958f59ef77dbc20cdec8df828b0b37fd
Fuma Motoyama <f.motoyama@gmail.com>
2023-01-22 23:09:52 +0900

    fix

--- __init__.py
+++ __init__.py
@@ -5,5 +5,5 @@ Created on Sun Jan 22 22:58:38 2023
 @author: fmotoyama
 """
 
-from env import windbg
-from env.env import Env, get_emmu_Handles
+from .env import windbg
+from .env.env import Env, get_emmu_Handles
